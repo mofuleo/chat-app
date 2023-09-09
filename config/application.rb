fce3d2db0cecb6c3062b242dba4b0828f3e04722
mofuleo <masakoterui12@gmail.com>
2023-09-09 15:42:25 +0900

    フロント実装の準備

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module ChatApp
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
-      f.helper false
+      g.helper false
       g.test_framework false
     end
   end
