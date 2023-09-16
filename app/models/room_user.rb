87f67f8caee58911bb6e862138b12a6d7fb9f398
mofuleo <masakoterui12@gmail.com>
2023-09-16 13:35:37 +0900

    メッセージの表示

--- app/models/room_user.rb
+++ app/models/room_user.rb
@@ -1,7 +1,7 @@
 class RoomUser < ApplicationRecord
- belongs_to :user
- belongs_to :room
+  belongs_to :room
+  belongs_to :user
  
 
-  validates :name, presence: true
+  # validates :name, presence: true
 end
